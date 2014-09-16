a7d5f1540213b7c2870a3369112c75b630fc00b9
GPFAFF <greg.pfaff@gmail.com>
2014-09-16 15:03:31 -0500

    adding changes

--- js/app.js
+++ js/app.js
@@ -1,77 +1,31 @@
-//var listItems = 1;
 
-//$(document).ready(function() {
+$(document).ready(function() {
 
-	//$('#add').on('keypress', function (event) {
-	//	if (event.which == '13'){
+	$('input').on('keypress', function (event) {
+		if (event.which == '13'){
 			// add new item
 			// alert('enter is hit');
-		//	addItem($(this).val());
-		//	$(this).val('');
-	//	}
-	//});
-
-	//$('.Add').click(function() {
-			// add new item
-	//		addItem($('#item').val());
-//			$('#item').val('');
-//	});
-
-//	$('ul').on('click', 'li', function() {
-		// alert('clicked on item');
-		// put middle line
-//		$(this).toggleClass('completed');
-//	});
-
-//	$('ul').on('click', '.delete', function(e) {
-//		e.stopImmediatePropagation();
-		// alert('clicked on DELETE');
-//		$(this).closest('li').remove();
-//		$(this).remove();
-//		if (!$('li').length){
-	//		$('.checkedItems').hide();
-//			first_item_added = 1;
-//		}
-//	});
-
-// });
-
-/* ======================
-   add item
-   ====================== */
-
-//function addItem(item){
-//	if (item){
-//		if (first_item_added){
-//			$('.checkedItems').show();
-//			listItems = 0;
-//		}
-//		$('.checkedItems').find('ul').append('<li>'+item+'<div href="#" class="delete">X</div></li>');
-	
-//}
-//}
-
-//$(document).ready(function() {
-
-	//$('#add').on('keypress', function (event) {
-	//	if (event.which == '13'){
-			// add new item
-			// alert('enter is hit');
-		//	addItem($(this).val());
-		//	$(this).val('');
-	//	}
-	//});
+			addItem($(this).val());
+			$(this).val('');
+		}
+	});
 
 $(document).ready(function(){
 	$(".Add").click(function(){
 		var toAdd = $("input[name=add-items]").val();
-		$(".checkedItems").append("<div class='checkedItems li'>"+toAdd+"</div>");
+		$("ul.checkedItems").append($("<div class='checkedItems'><li>"+toAdd+"</li></div>"));
 		$("input[name=add-items]").val("")
 	});
-	$(document).on("click",".checkedItems",function(){
-		$(this).toggleClass(".highlight")
-	});
-	$(document).on("dblclick",".checkedItems",function(){
+	
+	$(document).on('click', ".Remove", function(){
+  	$("ul.checkedItems").remove();
+  })
+	$(document).on("dblclick",".checkedItems li",function(){
 		$(this).remove()
 	})
 });
+});
+
+
+
+ 
