e0b2d29ef973b8481d54428b0d4d324c724a1715
GPFAFF <greg.pfaff@gmail.com>
2014-09-17 08:51:01 -0500

    adding javascript

--- js/app.js
+++ js/app.js
@@ -23,17 +23,17 @@ $(document).ready(function() {
 
 $(document).ready(function(){
 	$(".Add").click(function(){
-var toAdd = $("input[name=add-items]").val();
-$("ul.checkedItems").append($("<div class='checkedItems'><li>"+toAdd+"</li></div>"));
-$("input[name=add-items]").val("")
-});
-	$(document).on('click', ".Remove", function(){
-  	$("ul.checkedItems").remove();
-  })
-	$(document).on("dblclick",".checkedItems li",function(){
-		$(this).remove()
-	})
-});
-});
+		var toAdd = $("input[name=add-items]").val();
+			$("ul.checkedItems").append($("<div class='checkedItems'><li>"+toAdd+"</li></div>"));
+			$("input[name=add-items]").val("")
+		});
+				$(document).on('click', ".Remove", function(){
+  				$("ul.checkedItems").remove();
+  		})
+				$(document).on("dblclick",".checkedItems li",function(){
+				$(this).remove()
+				})
+		});
+	});
 
  
